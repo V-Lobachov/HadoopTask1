f08d49348e1860667519a5943768abe8a0662e30
V-Lobachov <hiken127@gmail.com>
2015-11-03 15:48:57 +0200

    Package mismatch solution + driver config modifications

--- src/main/java/com/epam/preprod/hadoop/AccessLogDriver.java
+++ src/main/java/com/epam/preprod/hadoop/AccessLogDriver.java
@@ -4,9 +4,11 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -26,12 +28,17 @@ public class AccessLogDriver extends Configured implements Tool {
         job.setJarByClass(AccessLogDriver.class);
 
         job.setMapperClass(AccessLogMapper.class);
+        job.setReducerClass(AccessLogReducer.class);
 
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(LongWritable.class);
 
-        TextInputFormat.addInputPath(job, new Path(args[0]));
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
 
+
+        TextInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
